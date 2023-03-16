bce5dfe4f7adb52b5d57ba58c8be71f1a12b0bda
crispri_g <mary.zackopiryna@yandex.ru>
2023-03-16 22:59:09 +0300

    gg

--- extra_tasks_1/thread_pool/task.h
+++ extra_tasks_1/thread_pool/task.h
@@ -28,13 +28,70 @@
 
 class ThreadPool {
 public:
-    ThreadPool(size_t threadCount) {}
+    ThreadPool(size_t threadCount): IsActive_(true), Terminate_without_Wait(false) {
+        for(size_t i = 0; i<threadCount;++i){
+            Pool_.emplace_back([this](){
+                while (true){
+                    std::unique_lock<std::mutex> lock(Mutex_);
+                    if(Terminate_without_Wait){
+                        while(!Tasks_Queue_.empty())
+                            Tasks_Queue_.pop();
+                        Mutex_.unlock();
+                        break;
+                    }
+                    else {
+                        if(!Tasks_Queue_.empty()) {
+                            std::function<void()> task = std::move( Tasks_Queue_.front());
+                            Tasks_Queue_.pop();
+                            Mutex_.unlock();
+                            task();
 
-    void PushTask(const std::function<void()>& task) {}
+                        }
+                        else{
+                            if(!IsActive())
+                                break;
+                        }
+                    }
+                }
 
-    void Terminate(bool wait) {}
+            });
+        }
+    }
 
-    bool IsActive() const {}
+    void PushTask(const std::function<void()>& task) {
+        std::unique_lock<std::mutex> lock(Mutex_);
+        if(IsActive()){
+            Tasks_Queue_.push(task);
+        }
+        else
+            throw std::exception();
+    }
 
-    size_t QueueSize() const {}
+    void Terminate(bool wait) {
+        std::unique_lock<std::mutex> lock(Mutex_);
+        IsActive_ = false;
+        if(!wait){
+            Terminate_without_Wait = true;
+        }
+        Mutex_.unlock();
+        for(auto& it:Pool_){
+            it.join();
+        }
+    }
+
+    bool IsActive() const {
+        return IsActive_.load();
+    }
+
+    size_t QueueSize() const {
+        std::unique_lock<std::mutex> lock(Mutex_);
+        return Tasks_Queue_.size();
+    }
+
+private:
+    mutable std::mutex Mutex_;
+    std::vector<std::thread> Pool_;
+    std::queue<std::function<void()>> Tasks_Queue_;
+    std::atomic<bool>IsActive_;
+    std::atomic<bool>Terminate_without_Wait;
 };
