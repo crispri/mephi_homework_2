9bc2d90cc57ed7a1997c7d68e2764eeda161d6ef
crispri_g <mary.zackopiryna@yandex.ru>
2023-03-16 13:56:53 +0300

    ggg

--- condition_variables/unbuffered_channel/task.h
+++ condition_variables/unbuffered_channel/task.h
@@ -28,29 +28,27 @@ public:
         while(!was_Read){
             Cv_Readble.wait(lock);
         }
-        // was_Read = false;
-
     }
 
     T Get(std::chrono::milliseconds timeout = std::chrono::milliseconds(0)) {
-        std::unique_lock lock(Mutex_);
         auto start = std::chrono::steady_clock::now();
+        std::unique_lock lock(Mutex_);
         while (Count_ == 0) {
             if (timeout == std::chrono::milliseconds(0)) {
                 Cv_Empty_.wait(lock);
             } else {
                 if (Cv_Empty_.wait_until(lock, timeout + start) == std::cv_status::timeout) {
                     Count_ = 0;
-                    Cv_Full_.notify_one();
                     was_Read = true;
+                    Cv_Full_.notify_one();
                     Cv_Readble.notify_one();
                     throw TimeOut();
                 }
             }
         }
         Count_ = 0;
-        Cv_Full_.notify_one();
         was_Read = true;
+        Cv_Full_.notify_one();
         Cv_Readble.notify_one();
         return Data_;
     }
